cc06c044c48075179d9d52d8a8ccc0478420951f
denis_chernushhenko <denis_chernushhenko@office.targem.ru>
2025-03-10 11:12:24 +0500

    Refactor performance tests.

--- ValueTypeLibrary.Tests/ValueType_PerformanceTests.cs
+++ ValueTypeLibrary.Tests/ValueType_PerformanceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using NUnit.Framework;
 using ValueTypeLibrary.Tests.TestValueTypes;
@@ -10,44 +11,64 @@ namespace ValueTypeLibrary.Tests;
 [TestFixture]
 public class ValueType_PerformanceTests
 {
+    private const int Count = 50_000_000;
+
+    private static readonly ImmutableArray<PersonName> personNames;
+    private static readonly ImmutableArray<PersonName_WithHandCodedMethods> handCodedPersonNames;
+
+    static ValueType_PerformanceTests()
+    {
+        personNames =
+            Enumerable.Range(1, Count)
+                      .Select(n => new PersonName(new string('f', n % 10), new string('s', n % 10)))
+                      .ToImmutableArray();
+        
+        handCodedPersonNames =
+            Enumerable.Range(1, Count)
+                      .Select(n => new PersonName_WithHandCodedMethods(new string('f', n % 10), new string('s', n % 10)))
+                      .ToImmutableArray();
+    }
+    
     [Test]
     public void EqualsPerformance()
     {
-        var count = 50_000_000;
         new PersonName("", "").Equals(new PersonName("", ""));
         new PersonName_WithHandCodedMethods("", "").Equals(new PersonName_WithHandCodedMethods("", ""));
-        var people1 = Enumerable.Range(1, count).Select(i => new PersonName(new string('f', i % 10), new string('s', i % 10))).ToList();
-        List<PersonName_WithHandCodedMethods?> people2 = Enumerable.Range(1, count).Select(i => new PersonName_WithHandCodedMethods(new string('f', i % 10), new string('s', i % 10))).ToList();
+        
         var sw = Stopwatch.StartNew();
-        for (var i = 0; i < people1.Count; i++)
+        for (var i = 0; i < Count; i++)
         {
-            people1[i].Equals(people1[people1.Count - 1 - i]);
+            personNames[i].Equals(personNames[Count - 1 - i]);
         }
-
         Console.WriteLine("ValueType<T> Equals: " + sw.Elapsed.TotalSeconds);
+        
         sw.Restart();
-        for (var i = 0; i < people2.Count; i++)
+        for (var i = 0; i < Count; i++)
         {
-            people2[i].Equals(people2[people2.Count - 1 - i]);
+            handCodedPersonNames[i].Equals(handCodedPersonNames[Count - 1 - i]);
         }
         Console.WriteLine("Hand coded Equals:   " + sw.Elapsed.TotalSeconds);
     }
     
     [Test]
+    [SuppressMessage("ReSharper", "ReturnValueOfPureMethodIsNotUsed")] // Calling for testing purposes
     public void GetHashCodePerformance()
     {
-        var count = 50_000_000;
         new PersonName("", "").GetHashCode();
         new PersonName_WithHandCodedMethods("", "").GetHashCode();
-        var people1 = Enumerable.Range(1, count).Select(i => new PersonName(new string('f', i % 10), new string('s', i % 10))).ToList();
-        var people2 = Enumerable.Range(1, count).Select(i => new PersonName_WithHandCodedMethods(new string('f', i % 10), new string('s', i % 10))).ToList();
+        
         var sw = Stopwatch.StartNew();
-        foreach (var person in people1)
-            person.GetHashCode();
+        foreach (var personName in personNames)
+        {
+            personName.GetHashCode();
+        }
         Console.WriteLine("ValueType<T> GetHashCode: " + sw.Elapsed.TotalSeconds);
+        
         sw.Restart();
-        foreach (var person in people2)
-            person.GetHashCode();
+        foreach (var handCodedPersonName in handCodedPersonNames)
+        {
+            handCodedPersonName.GetHashCode();
+        }
         Console.WriteLine("Hand coded GetHashCode:   " + sw.Elapsed.TotalSeconds);
     }
 }
